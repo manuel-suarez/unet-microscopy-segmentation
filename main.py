3142951f8a3634be5fbce396e7870f2485ac3ef4
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-31 13:16:52 -0600

    Define work directories

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ Mitochondria semantic segmentation using U-net, Attention Unet and Att Res Unet
 import os
 
 home_dir = os.path.expanduser('~')
+base_dir = os.path.join(home_dir, 'data')
+work_dir = os.path.join(base_dir, 'microscopy-dataset')
+train_dir = os.path.join(work_dir, 'training')
+
+image_dir = os.path.join(train_dir, 'images')
+mask_dir = os.path.join(train_dir, 'masks')
