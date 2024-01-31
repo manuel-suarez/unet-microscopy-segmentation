4b0e1d4f27d38e03fa96bae72a6976760597f7d6
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-31 15:43:37 -0600

    Training UNet

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
+from datetime import datetime
 from matplotlib import pyplot as plt
+from tensorflow.keras.optimizers import Adam
 
 home_dir = os.path.expanduser('~')
 base_dir = os.path.join(home_dir, 'data')
@@ -54,3 +56,34 @@ plt.subplot(122)
 plt.imshow(np.reshape(y_train[image_number], (256, 256)), cmap='gray')
 plt.show()
 
+# Parameters for model
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
+num_labels = 1
+input_shape = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+batch_size = 8
+
+# Use library implementation
+from focal_loss import BinaryFocalLoss
+
+from models import UNet, dice_coef, dice_loss, IoU_coef
+
+unet_model = UNet(input_shape)
+unet_model.compile(optimizer=Adam(lr = 1e-2), loss=BinaryFocalLoss(gamma=2),
+                   metrics=['accuracy', IoU_coef])
+print(unet_model.summary())
+
+start1 = datetime.now()
+unet_history = unet_model.fit(X_train, y_train,
+                              verbose=1,
+                              batch_size=batch_size,
+                              validation_data=(X_test, y_test),
+                              shuffle=False,
+                              epochs=50)
+stop1 = datetime.now()
+# Execution time of the model
+execution_time_Unet = stop1-start1
+print("UNet execution time is: ", execution_time_Unet)
+
+unet_model.save('mitochondria_UNet_50epochs_B_focal.hdf5')
