60342237c4d272cb4d85be6baa4aa31493469b02
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-02 21:12:34 -0600

    Reorganize model

--- models/functional/unet.py
+++ models/functional/unet.py
@@ -38,38 +38,38 @@ def UNet(input_shape=(256, 256, 1), batch_norm=True, dropout_rate=0.0):
     inputs = layers.Input(input_shape)
     # Encoder Path: 256 -> 128 -> 64 -> 32 -> 16 -> 8
     # Encoder Block1 : convolutional + max pool : 256 -> 128
-    block128 = convolutional_block(inputs, 64, batch_norm, dropout_rate)
-    pool64   = layers.MaxPooling2D(pool_size=(2,2))(block128)
+    dn_128 = convolutional_block(inputs, 64, batch_norm, dropout_rate)
+    pl_64 = layers.MaxPooling2D(pool_size=(2,2))(dn_128)
     # Encoder Block2 : convolutional + max pool : 128 -> 64
-    block64  = convolutional_block(pool64, 128, batch_norm, dropout_rate)
-    pool32   = layers.MaxPooling2D(pool_size=(2,2))(block64)
+    dn_64 = convolutional_block(pl_64, 128, batch_norm, dropout_rate)
+    pl_32 = layers.MaxPooling2D(pool_size=(2,2))(dn_64)
     # Encoder Block3 : convolutional + max pool : 64 -> 32
-    block32  = convolutional_block(pool32, 256, batch_norm, dropout_rate)
-    pool16   = layers.MaxPooling2D(pool_size=(2,2))(block32)
+    dn_32 = convolutional_block(pl_32, 256, batch_norm, dropout_rate)
+    pl_16 = layers.MaxPooling2D(pool_size=(2,2))(dn_32)
     # Encoder Block4 : convolutional + max pool : 32 -> 16
-    block16  = convolutional_block(pool16, 512, batch_norm, dropout_rate)
-    pool8    = layers.MaxPooling2D(pool_size=(2,2))(block16)
+    dn_16 = convolutional_block(pl_16, 512, batch_norm, dropout_rate)
+    pl_8 = layers.MaxPooling2D(pool_size=(2,2))(dn_16)
     # Bottleneck
-    block8   = convolutional_block(pool8, 1024, batch_norm, dropout_rate)
+    dn_8 = convolutional_block(pl_8, 1024, batch_norm, dropout_rate)
     # Decoder path: 8 -> 16 -> 32 -> 64 -> 128 -> 256
     # Decoder Block4 : upsample + concat + convolutional : 8 -> 16
-    up_block16 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(block8)
-    concat16   = layers.concatenate([up_block16, block16], axis=3)
-    dec_block16 = convolutional_block(concat16, 512, batch_norm, dropout_rate)
+    up_16 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dn_8)
+    ct_16 = layers.concatenate([up_16, dn_16], axis=3)
+    dc_16 = convolutional_block(ct_16, 512, batch_norm, dropout_rate)
     # Decoder Block3 : upsample + concat + convolutional : 16 -> 32
-    up_block32 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dec_block16)
-    concat32 = layers.concatenate([up_block32, block32], axis=3)
-    dec_block32 = convolutional_block(concat32, 256, batch_norm, dropout_rate)
+    up_32 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dc_16)
+    ct_32 = layers.concatenate([up_32, dn_32], axis=3)
+    dc_32 = convolutional_block(ct_32, 256, batch_norm, dropout_rate)
     # Decoder Block2 : upsample + concat + convolutional : 32 -> 64
-    up_block64 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dec_block32)
-    concat64 = layers.concatenate([up_block64, block64], axis=3)
-    dec_block64 = convolutional_block(concat64, 128, batch_norm, dropout_rate)
+    up_64 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dc_32)
+    ct_64 = layers.concatenate([up_64, dn_64], axis=3)
+    dc_64 = convolutional_block(ct_64, 128, batch_norm, dropout_rate)
     # Decoder Block1 : upsample + concat + convolutional : 64 -> 128
-    up_block128 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dec_block64)
-    concat128 = layers.concatenate([up_block128, block128], axis=3)
-    dec_block128 = convolutional_block(concat128, 64, batch_norm, dropout_rate)
+    up_128 = layers.UpSampling2D(size=(2, 2), data_format="channels_last")(dc_64)
+    ct_128 = layers.concatenate([up_128, dn_128], axis=3)
+    dc_128 = convolutional_block(ct_128, 64, batch_norm, dropout_rate)
     # Classification layer
-    outputs = layers.Conv2D(1, kernel_size=(1,1))(dec_block128)
+    outputs = layers.Conv2D(1, kernel_size=(1,1))(dc_128)
     outputs = layers.BatchNormalization(axis=3)(outputs)
     outputs = layers.Activation('sigmoid')(outputs)
 
